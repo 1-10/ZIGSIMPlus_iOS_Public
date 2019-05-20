492586bea105afe4b08d536c7edaf24286124f10
Yoneyama <yoneyama@1-10.com>
2019-05-20 15:13:26 +0900

    add pressure command

---
+++ ZIGSIMPlus/Models/Commands/PressureMonitoringCommand.swift
@@ -0,0 +1,9 @@
+//
+//  PressureMonitoringCommand.swift
+//  ZIGSIMPlus
+//
+//  Created by YoneyamaShunpei on 2019/05/20.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import Foundation
