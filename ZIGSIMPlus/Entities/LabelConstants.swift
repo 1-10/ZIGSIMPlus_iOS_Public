3fc22ad509195446aec35a56ff644827ac713229
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-05-21 20:37:39 +0900

    Separate Command and CommandData
    CommandData represents setting items, and Command represents concrete function.
    CommandAndCommandDataMediator mediates Command, CommandData and Presenter.
    Getting gravity is not yet implemented.

--- ZIGSIMPlus/Entities/LabelConstants.swift
+++ ZIGSIMPlus/Entities/LabelConstants.swift
@@ -9,12 +9,8 @@
 import Foundation
 
 public struct LabelConstants {
-    public static let accelaration = "Acceleration"
+    public static let acceleration = "Acceleration"
+    public static let gravity = "Gravity"
     public static let battery = "Battery"
-    public static let autoUpdatedCommands: [String] = [accelaration]
-    public static let manualUpdatedCommands: [String] = [battery]
-    
-    public static var commands: [String] {
-        return autoUpdatedCommands + manualUpdatedCommands
-    }
+    public static let commandDatas: [String] = [acceleration, gravity, battery]
 }
