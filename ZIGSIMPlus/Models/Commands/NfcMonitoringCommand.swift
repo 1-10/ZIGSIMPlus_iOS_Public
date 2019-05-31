f5a3b0f39105f952dfc58b3bb39d6f6e86e51381
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-05-31 11:42:20 +0900

    Add NfcMonitoringCommand
    Currently it's empty.

---
+++ ZIGSIMPlus/Models/Commands/NfcMonitoringCommand.swift
@@ -0,0 +1,26 @@
+//
+//  NfcMonitoringCommand.swift
+//  ZIGSIMPlus
+//
+//  Created by Nozomu Kuwae on 2019/05/30.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import Foundation
+import CoreNFC
+
+public final class NfcMonitoringCommand: AutoUpdatedCommand {
+    public func isAvailable() -> Bool {
+        <#code#>
+    }
+    
+    public func start(completion: ((String?) -> Void)?) {
+        <#code#>
+    }
+    
+    public func stop(completion: ((String?) -> Void)?) {
+        <#code#>
+    }
+    
+    
+}
