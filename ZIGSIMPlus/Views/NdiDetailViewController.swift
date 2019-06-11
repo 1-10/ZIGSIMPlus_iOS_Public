f2c554700ab8333e50229154a56e4ac6583cfad7
Yoneyama <yoneyama@1-10.com>
2019-06-11 20:00:28 +0900

    update

--- ZIGSIMPlus/Views/NdiDetailViewController.swift
+++ ZIGSIMPlus/Views/NdiDetailViewController.swift
@@ -8,17 +8,48 @@
 
 import Foundation
 import UIKit
+import SwiftyUserDefaults
 
 public class NdiDetailViewController : UIViewController {
     
     @IBOutlet weak var segmentLabel: UILabel!
-    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet var segments: [UISegmentedControl]!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
+        for segment in segments {
+            if segment.tag == 0 {
+                segment.selectedSegmentIndex = Int(Defaults[.userNdiType]?.description ?? "0") ?? 0
+            } else if segment.tag == 1 {
+                segment.selectedSegmentIndex = Int(Defaults[.userNdiCameraType]?.description ?? "0") ?? 0
+            } else if segment.tag == 2 {
+                segment.selectedSegmentIndex = Int(Defaults[.userDepthType]?.description ?? "0") ?? 0
+            }
+        }
     }
     
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
-        
+        if sender.tag == 0 { // CAMERA or DEPTH
+            if sender.selectedSegmentIndex == 0 {
+                AppSettingModel.shared.ndiType = .CAMERA
+            } else if sender.selectedSegmentIndex == 1 {
+                AppSettingModel.shared.ndiType = .DEPTH
+            }
+            Defaults[.userNdiType] = AppSettingModel.shared.ndiType.rawValue
+        } else if sender.tag == 1 { // OUTCAMERA or INCAMERA
+            if sender.selectedSegmentIndex == 0 {
+                AppSettingModel.shared.ndiCameraType = .BACK
+            } else if sender.selectedSegmentIndex == 1 {
+                AppSettingModel.shared.ndiCameraType = .FRONT
+            }
+            Defaults[.userNdiCameraType] = AppSettingModel.shared.ndiCameraType.rawValue
+        } else if sender.tag == 2 { // DEPTH or DISPARITY
+            if sender.selectedSegmentIndex == 0 {
+                AppSettingModel.shared.depthType = .DEPTH
+            } else if sender.selectedSegmentIndex == 1 {
+                AppSettingModel.shared.depthType = .DISPARITY
+            }
+            Defaults[.userDepthType] = AppSettingModel.shared.depthType.rawValue
+        }
     }
 }
