37c191fdb5206d51b1da8fdeb09e3784151cea57
Yoneyama <yoneyama@1-10.com>
2019-06-13 11:13:13 +0900

    NDIの詳細画面をviewとpresenterに分割

--- ZIGSIMPlus/Views/NdiDetailViewController.swift
+++ ZIGSIMPlus/Views/NdiDetailViewController.swift
@@ -8,48 +8,35 @@
 
 import Foundation
 import UIKit
-import SwiftyUserDefaults
 
 public class NdiDetailViewController : UIViewController {
     
     @IBOutlet weak var segmentLabel: UILabel!
     @IBOutlet var segments: [UISegmentedControl]!
+    var presenter: NdiDetailPresenterProtocol!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
+        presenter = NdiDetailPresenter()
+        let userDefaultSegments = presenter.getUserDefault()
         for segment in segments {
             if segment.tag == 0 {
-                segment.selectedSegmentIndex = Int(Defaults[.userNdiType]?.description ?? "0") ?? 0
+                segment.selectedSegmentIndex = Int(userDefaultSegments[.NDI_TYPE] ?? 0)
             } else if segment.tag == 1 {
-                segment.selectedSegmentIndex = Int(Defaults[.userNdiCameraType]?.description ?? "0") ?? 0
+                segment.selectedSegmentIndex = Int(userDefaultSegments[.NDI_CAMERA_TYPE] ?? 0)
             } else if segment.tag == 2 {
-                segment.selectedSegmentIndex = Int(Defaults[.userDepthType]?.description ?? "0") ?? 0
+                segment.selectedSegmentIndex = Int(userDefaultSegments[.DEPTH_TYPE] ?? 0)
             }
         }
     }
     
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
         if sender.tag == 0 { // CAMERA or DEPTH
-            if sender.selectedSegmentIndex == 0 {
-                AppSettingModel.shared.ndiType = .CAMERA
-            } else if sender.selectedSegmentIndex == 1 {
-                AppSettingModel.shared.ndiType = .DEPTH
-            }
-            Defaults[.userNdiType] = AppSettingModel.shared.ndiType.rawValue
+            presenter.updateNdiTypeUserDefault(selectIndex: sender.selectedSegmentIndex)
         } else if sender.tag == 1 { // OUTCAMERA or INCAMERA
-            if sender.selectedSegmentIndex == 0 {
-                AppSettingModel.shared.ndiCameraType = .BACK
-            } else if sender.selectedSegmentIndex == 1 {
-                AppSettingModel.shared.ndiCameraType = .FRONT
-            }
-            Defaults[.userNdiCameraType] = AppSettingModel.shared.ndiCameraType.rawValue
+            presenter.updateNdiCameraTypeUserDefault(selectIndex: sender.selectedSegmentIndex)
         } else if sender.tag == 2 { // DEPTH or DISPARITY
-            if sender.selectedSegmentIndex == 0 {
-                AppSettingModel.shared.depthType = .DEPTH
-            } else if sender.selectedSegmentIndex == 1 {
-                AppSettingModel.shared.depthType = .DISPARITY
-            }
-            Defaults[.userDepthType] = AppSettingModel.shared.depthType.rawValue
+            presenter.updateDepthTypeUserDefault(selectIndex: sender.selectedSegmentIndex)
         }
     }
 }
