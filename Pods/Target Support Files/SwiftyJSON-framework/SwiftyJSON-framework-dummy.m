1f3bd59fc77fe6d1b1e38c2e89736deab490c5fb
fand <fand.gmork@gmail.com>
2019-06-13 12:16:07 +0900

    Install LicensePlist

---
+++ Pods/Target Support Files/SwiftyJSON-framework/SwiftyJSON-framework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftyJSON_framework : NSObject
+@end
+@implementation PodsDummy_SwiftyJSON_framework
+@end
---
+++ Pods/Target Support Files/SwiftyJSON-library/SwiftyJSON-library-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftyJSON_library : NSObject
+@end
+@implementation PodsDummy_SwiftyJSON_library
+@end
