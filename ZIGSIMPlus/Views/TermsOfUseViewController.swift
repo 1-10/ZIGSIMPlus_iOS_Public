11d75add9d1b44f438dc0ce6aca8ecba4ddfb708
Yoneyama <yoneyama@1-10.com>
2019-06-27 16:39:43 +0900

    利用規約をplistファイルに記述

--- ZIGSIMPlus/Views/TermsOfUseViewController.swift
+++ ZIGSIMPlus/Views/TermsOfUseViewController.swift
@@ -14,9 +14,12 @@ public class TermsOfUseViewController : UIViewController {
     @IBOutlet weak var text: UITextView!
     override public func viewDidLoad() {
         super.viewDidLoad()
+        let filePath = Bundle.main.path(forResource: "TermsOfUse", ofType:"plist" )
+        let plist = NSDictionary(contentsOfFile: filePath!)
+        let termsOfuse:String? = plist?["TermsOfUse"] as? String
         let markdownParser = MarkdownParser(font: UIFont.systemFont(ofSize: 10))
         markdownParser.bold.font = UIFont.systemFont(ofSize: 24)
-        text.attributedText = markdownParser.parse(termsOfUseText)
+        text.attributedText = markdownParser.parse(termsOfuse ?? "")
         text.textColor = Theme.main
     }
 }
