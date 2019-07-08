0b2417bfe9935f8961dc6869c0e436e7c7085e42
Yoneyama <yoneyama@1-10.com>
2019-07-08 17:31:03 +0900

    Fix commpass modal

--- ZIGSIMPlus/Entities/ModalTexts.swift
+++ ZIGSIMPlus/Entities/ModalTexts.swift
@@ -29,7 +29,7 @@ Values are quaternion for X, Y, Z and W axis.
 """),
     .compass : ("Compass", """
 Compass command detects azimuth angle and outputs following values:
-**Compass**:North is 0 degrees, east is 90 degrees, south is 180 degrees, and so on. A negative value indicates an invalid direction.
+**Compass**:Azimuth angle.
 **Orientation**:Orientation of device.This can be set in the detail settings.
 
 Users can switch the orientation of device in detail settings:
