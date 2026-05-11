3c4fdc5ab6081c1073cb04985c59346ef782858c
harry00902 <harry@1-10.com>
2026-05-11 16:06:40 +0900

    Issue #208 Replace StandardCell VC reference with delegate

--- ZIGSIMPlus/Views/CustomCells/StandardCell.swift
+++ ZIGSIMPlus/Views/CustomCells/StandardCell.swift
@@ -9,6 +9,13 @@
 import Foundation
 import UIKit
 
+protocol StandardCellDelegate: AnyObject {
+    func showDetail(commandNo: Int)
+    func showModal(commandNo: Int)
+    func setSelectedButtonAvailable(_ selectedCommandNo: Int)
+    func setNdiArkitImageDetectionButtonAvailable()
+}
+
 public class StandardCell: UITableViewCell {
     @IBOutlet var commandOnOffButton: UIButton!
     @IBOutlet var commandLabel: UILabel!
@@ -17,28 +24,26 @@ public class StandardCell: UITableViewCell {
     @IBOutlet var detailImageView: UIImageView!
     @IBOutlet var modalButton: UIButton!
     @IBOutlet var labelConstaint: NSLayoutConstraint!
-    var commandSelectionPresenter: CommandSelectionPresenterProtocol!
-    var viewController: UIViewController!
+    weak var commandSelectionPresenter: CommandSelectionPresenterProtocol?
+    weak var delegate: StandardCellDelegate?
 
     @IBAction func commandOnOffButtonAction(_ sender: UIButton) {
         setCheckMarkLabelText()
         setCommandsOfImageAvailability(sender)
         if checkMarkLabel.text == checkMark {
-            commandSelectionPresenter.saveCommandOnOffToUserDefaults(Command.allCases[sender.tag], true)
+            commandSelectionPresenter?.saveCommandOnOffToUserDefaults(Command.allCases[sender.tag], true)
         } else {
-            commandSelectionPresenter.saveCommandOnOffToUserDefaults(Command.allCases[sender.tag], false)
+            commandSelectionPresenter?.saveCommandOnOffToUserDefaults(Command.allCases[sender.tag], false)
         }
-        commandSelectionPresenter.didSelectRow(atLabel: Command.allCases[sender.tag].rawValue)
+        commandSelectionPresenter?.didSelectRow(atLabel: Command.allCases[sender.tag].rawValue)
     }
 
     @IBAction func detailButtonAction(_: UIButton) {
-        guard let parent = viewController as? CommandSelectionViewController else { return }
-        parent.showDetail(commandNo: commandOnOffButton.tag)
+        delegate?.showDetail(commandNo: commandOnOffButton.tag)
     }
 
     @IBAction func modalButtonAction(_: UIButton) {
-        guard let parent = viewController as? CommandSelectionViewController else { return }
-        parent.showModal(commandNo: commandLabel.tag)
+        delegate?.showModal(commandNo: commandLabel.tag)
     }
 
     func initCell() {
@@ -52,14 +57,13 @@ public class StandardCell: UITableViewCell {
     }
 
     func setCommandsOfImageAvailability(_ sender: UIButton) {
-        guard let parent = viewController as? CommandSelectionViewController else { return }
         if Command.allCases[sender.tag] == .ndi ||
             Command.allCases[sender.tag] == .arkit ||
             Command.allCases[sender.tag] == .imageDetection {
             if checkMarkLabel.text == checkMark {
-                parent.setSelectedButtonAvailable(sender.tag)
+                delegate?.setSelectedButtonAvailable(sender.tag)
             } else {
-                parent.setNdiArkitImageDetectionButtonAvailable()
+                delegate?.setNdiArkitImageDetectionButtonAvailable()
             }
         }
     }
